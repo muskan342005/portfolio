a1415add9e9e451a279ecd37eaacc642ddc6f6d2
Abdul Rauf <rauf22750@gmail.com>
2025-02-18 20:36:55 +0500

    first commit

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'portfolio/static'),  # Update this path to match your structure
+    os.path.join(BASE_DIR, 'static'),  # Update this path to match your structure
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
