b8ec997e52371d8c067c877379952b29b336aaa6
Abdul Rauf <rauf22750@gmail.com>
2025-02-18 20:24:34 +0500

    first commit

--- myportfolio/wsgi.py
+++ myportfolio/wsgi.py
@@ -9,10 +9,8 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myportfolio.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root='staticfiles')
-app = application
+app = application  # This line is important for Vercel
