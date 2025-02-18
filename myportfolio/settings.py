48446233a5c352eae19bcbc7453173099db7b9cc
Abdul Rauf <rauf22750@gmail.com>
2025-02-18 22:02:29 +0500

    first commit

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -125,6 +125,7 @@ STATICFILES_DIRS = [
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+WHITENOISE_MANIFEST_STRICT = False
