24a9c86d26006c346565490bcc8bcd03fac01528
Abdul Rauf <rauf22750@gmail.com>
2025-02-18 21:49:34 +0500

    first commit

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -48,6 +48,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Add this line
+
 ]
 
 ROOT_URLCONF = 'myportfolio.urls'
@@ -125,3 +127,4 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
