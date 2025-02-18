1a2be7633b296108f2544c4181a9b4b31ff57956
Abdul Rauf <rauf22750@gmail.com>
2025-02-18 22:58:27 +0500

    first commit

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -124,6 +124,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  # Update this path to match your structure
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 BASE_DIR = Path(__file__).resolve().parent.parent
--- myportfolio/wsgi.py
+++ myportfolio/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myportfolio.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'win.settings')
 
 application = get_wsgi_application()
-app = application  # This line is important for Vercel
+
+app=application
